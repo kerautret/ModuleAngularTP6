a22c5214515fee30b2c5c5aa73d886d4e71e64e4
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2019-01-31 06:34:40 +0100

    import OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,6 +24,7 @@ import { ActiviteComponent } from './activite/activite.component';
     BrowserAnimationsModule,
     FormsModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [ActiviteService],
